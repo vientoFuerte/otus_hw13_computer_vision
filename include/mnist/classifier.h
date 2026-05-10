8a1a4c7159c8ba87d51380424d9888c0df019eb1
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-05-10 11:48:47 +0300

    Update classifier.h
    Добавлены комментарии.

--- include/mnist/classifier.h
+++ include/mnist/classifier.h
@@ -5,18 +5,19 @@
 
 namespace mnist {
 
+ /* aбстрактный базовый класс (интерфейс) для классификаторов */
 class Classifier {
 public:
-    using features_t = std::vector<float>;
-    using probas_t = std::vector<float>;
+    using features_t = std::vector<float>;  // вектор признаков (784 пикселя) 
+    using probas_t = std::vector<float>;    // вектор вероятностей принадлежности к каждому классу
 
     virtual ~Classifier() {}
 
-    virtual size_t num_classes() const = 0;
+    virtual size_t num_classes() const = 0;                // Возвращает количество классов, которые умеет распознавать классификатор(10).
 
-    virtual size_t predict(const features_t&) const = 0;
+    virtual size_t predict(const features_t&) const = 0;   // Предсказывает номер(0-9) класса для входных признаков
 
-    virtual probas_t predict_proba(const features_t&) const = 0;
+    virtual probas_t predict_proba(const features_t&) const = 0;  // Возвращает вероятности для каждого класса
 };
 
 
